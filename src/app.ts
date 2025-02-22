878dc6b45a870466e887117ec8b9d90bf4f09307
alessandrocdiasbr <acdias83@gmail.com>
2025-02-22 17:48:27 -0300

    app.ts att

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,30 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import phoneRoutes from './routes/phone.routes';
+import rechargeRoutes from './routes/recharge.routes';
+import { errorHandler } from './middlewares/error.middleware';
 
 dotenv.config();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
 app.get("/health", (req: express.Request, res: express.Response) => {
-    res.status(200).send();
+  res.status(200).send();
 });
 
 
+app.use(phoneRoutes);
+app.use(rechargeRoutes);
+
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
 
 export default app;
